d5e67778bf48a81319e2e64fb1de66d2de867eb9
ydjaghloul <younes.djaghloul@gmail.com>
2017-03-11 17:54:08 +0100

    Add the class Speech To Text

--- src/main/java/lu/arhs/odyssi/speech2text/Speech2Text.java
+++ src/main/java/lu/arhs/odyssi/speech2text/Speech2Text.java
@@ -1,9 +1,9 @@
 package lu.arhs.odyssi.speech2text;
 
 public interface Speech2Text {
-	String rawnLPQuery="";
+	String rawNLPQuery="";
 	String cleanNLPQuery="";
 	public String process(String input);
 	public String cleanNLPQuery(String rawQuery);
-
+	void setRawNLPQuery( String rawNLPQuery);
 }
