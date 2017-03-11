126363d5c0757736f3740715a81df3005d7ddb9e
ydjaghloul <younes.djaghloul@gmail.com>
2017-03-11 17:22:09 +0100

    Interface Description

--- src/main/java/lu/arhs/odyssi/speech2text/Speech2Text.java
+++ src/main/java/lu/arhs/odyssi/speech2text/Speech2Text.java
@@ -1,6 +1,9 @@
 package lu.arhs.odyssi.speech2text;
 
 public interface Speech2Text {
-	
+	String rawnLPQuery="";
+	String cleanNLPQuery="";
 	public String process(String input);
+	public String cleanNLPQuery(String rawQuery);
+
 }
