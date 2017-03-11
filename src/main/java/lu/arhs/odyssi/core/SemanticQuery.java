6f80243d9eee3852d8ff5b348775a3c0cb0b6ed4
Mathieu Bollen <mathieu.bollen@arhs-cube.com>
2017-03-11 17:06:45 +0100

    Semantic Query beans

--- src/main/java/lu/arhs/odyssi/core/SemanticQuery.java
+++ src/main/java/lu/arhs/odyssi/core/SemanticQuery.java
@@ -1,5 +1,14 @@
 package lu.arhs.odyssi.core;
 
+import java.util.Collection;
+
+/**
+ * @author bollenma
+ */
 public class SemanticQuery {
+
+    private Collection<ContextualKeyword> contextualKeywords;
+
+
 
 }
